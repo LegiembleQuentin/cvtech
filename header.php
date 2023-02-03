00ad2b94ace592b2da1054c781a9606f4a842753
Hyotre <97020497+Hyotre@users.noreply.github.com>
2023-02-03 16:28:50 +0100

    init footerr

--- header.php
+++ header.php
@@ -26,19 +26,33 @@
     <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'cvtech' ); ?></a>
 
     <header id="masthead" class="site-header bg-header-black padd-two">
+
         <div class="wrap1 header">
             <div class="logo-header">
                 <img src="./wp-content/themes/cvtech/inc/img/logo-CvTech.png" alt="">
             </div>
-            <nav>
+            <nav class="d-block">
                 <ul class="d-flex gaptwo">
-                    <li class="li-header"><a class="color-white box-yellow" href="" title="">Contactez-Nous</a></li>
-                    <li  class="li-header" ><a class="color-white box-yellow" href="" title="">Inscription</a></li>
-                    <li  class="li-header" ><a class="color-white box-yellow" href="" title="">Connexion</a></li>
-<!--                    <li><a class="color-white" href="" title="">Mon compte</a></li>-->
-<!--                    <li ><a class="color-white" href="" title="">Déco</a></li>-->
+
+                    <div class="gap d-flex d-block gaptwo ">
+                        <li class="li-header border-twenty"><a class="color-white box-yellow " href="" title="">Contactez-Nous</a></li>
+                        <li  class="li-header border-twenty" ><a class="color-white box-yellow " href="" title="">Inscription</a></li>
+                        <li  class="li-header border-twenty" ><a class="color-white box-yellow " href="" title="">Connexion</a></li>
+                        <!--                    <li><a class="color-white" href="" title="">Mon compte</a></li>-->
+                        <!--                    <li ><a class="color-white" href="" title="">Déco</a></li>-->
+                    </div>
                 </ul>
         </div>
         </nav>
+        <div class="d-none">
+            <button id="burger-menu-button">&#9776;</button>
+            <nav id="burger-menu">
+                <ul>
+                    <li><a href="#">Contactez-Nous</a></li>
+                    <li><a href="#">Inscription</a></li>
+                    <li><a href="#">Connexion</a></li>
+                </ul>
+            </nav>
+        </div>
     </header><!-- #masthead -->
 
---
+++ inc/js/burger.js
@@ -0,0 +1,6 @@
+    const button = document.getElementById('burger-menu-button');
+    const menu = document.getElementById('burger-menu');
+
+    button.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
