0ad6798104751f2ede3dc54294f0475966398083
Hyotre <97020497+Hyotre@users.noreply.github.com>
2023-02-03 11:08:28 +0100

    init hedeé

--- header.php
+++ header.php
@@ -13,47 +13,30 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'cvtech' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'cvtech' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$cvtech_description = get_bloginfo( 'description', 'display' );
-			if ( $cvtech_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $cvtech_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+    <header id="masthead" class="site-header bg-header-black">
+        <div class="wrap-header">
+            <div>
+                <img src="" alt="">
+            </div>
+            <nav class="">
+                <ul>
+                    <li><a href="" title="">Contactez-Nous</a></li>
+                    <li class="homelink"><a href="" title="">Inscription</a></li>
+                    <li class="homelink"><a href="" title="">Connexion</a></li>
+                </ul>
+        </div>
+        </nav>
+    </header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cvtech' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
---
+++ template-homepage.php
@@ -0,0 +1,10 @@
+<?php
+/* Template Name: homepage */
+get_header();
+?>
+<section>
+    <p> aaaaaaaaaaaaaaaaaaaaa </p>
+    <p> aaaaaaaaaaaaaaaaaaaaa </p>
+    <p> aaaaaaaaaaaaaaaaaaaaa </p>
+    <p> aaaaaaaaaaaaaaaaaaaaa </p>
+</section>
