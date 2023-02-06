bb2f51d50c73418f522ff76be7c4ccfe1d613c55
Hyotre <97020497+Hyotre@users.noreply.github.com>
2023-02-06 16:00:09 +0100

    footeru

--- template-contact.php
+++ template-contact.php
@@ -1,14 +1,6 @@
 <?php
 /*Template Name: contact*/
 get_header(); ?>
-<div class="logo-header">
-    <img src="<?= asset('/img/left_tache.svg')?>" alt="">
-
-</div>
-<div class="logo-header">
-    <img src="<?= asset('/img/right_tache.svg')?>" alt="">
-
-</div>
 
 
 
