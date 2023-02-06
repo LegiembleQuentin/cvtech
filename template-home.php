2bdd17533b59d0170457edc5142b20e2ba053e76
Petyth_prince <petythprincebely@gmail.com>
2023-02-06 10:07:43 +0100

    recuperation du header

--- template-home.php
+++ template-home.php
@@ -1,1 +1,8 @@
 <?php
+/*Template Name: homepage*/
+get_header(); ?>
+
+<?php get_template_part('view/home/home', 'intro'); ?>
+
+<?php get_footer() ?>
+
---
+++ view/home/home-intro.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+
+<section id="intro" class="wrap">
+    <div class="left">
+        <div class="txt_intro">
+            <h1>cvtech</h1>
+            <p>Bienvenue sur notre plateforme de gestion des CV en ligne, dédié au recrutement de techniciens qualifiés dans les domaines de l’informatique et du numérique.</p>
+        </div>
+        <div class="link_intro">
+            <a href="">Creer votre compte</a>
+        </div>
+    </div>
+</section>
