4bc8a8cde95fe2c48a2e595bc76a3bd8e090e8c9
LegiembleQuentin <q.legiemble@gmail.com>
2023-02-06 11:32:16 +0100

    merge quentin

--- inc/generale.php
+++ inc/generale.php
@@ -53,15 +53,18 @@ add_action( 'widgets_init', 'cvtech_widgets_init' );
 function cvtech_scripts() {
     //CSS
     wp_enqueue_style( 'cvtech-style', get_stylesheet_uri(), array(), _S_VERSION );
+
     if(is_page_template('template-home.php')){
         wp_enqueue_style('flexslider-css', get_template_directory_uri() . '/asset/flexslider/flexslider.css', array(), _S_VERSION);
+    }
+
+    if(is_page_template( 'template-cgu.php' )) {
+        wp_enqueue_style('cgu-css', get_template_directory_uri() . 'inc/asset/css/cgu.css', array(), _S_VERSION);
     }
 
     //JS
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js', array(), _S_VERSION, true);
-
-    wp_enqueue_script( 'cvtech-burger', get_template_directory_uri() . '/asset/js/burger.js', array(), _S_VERSION, true );
 
     wp_add_inline_script( 'jquery', 'const MYSCRIPT = ' . json_encode( array(
             'ajaxUrl' => admin_url( 'admin-ajax.php' ),
@@ -69,6 +72,7 @@ function cvtech_scripts() {
             'theme'   => get_template_directory_uri(),
         ) ), 'before' );
 
+    wp_enqueue_script( 'cvtech-burger', get_template_directory_uri() . '/asset/js/burger.js', array(), _S_VERSION, true );
     wp_enqueue_script('function-js', get_template_directory_uri() . '/asset/js/function.js', array(), _S_VERSION, true);
 
     if(is_page_template('template-home.php')){
@@ -79,20 +83,8 @@ function cvtech_scripts() {
         wp_enqueue_script('register-js', get_template_directory_uri() . '/asset/js/register.js', array(), _S_VERSION, true);
     }
 
-=======
-    wp_enqueue_style( 'cvtech-style', get_stylesheet_uri(), array(), _S_VERSION );
-
-    wp_style_add_data( 'cvtech-style', 'rtl', 'replace' );
-
-    wp_enqueue_script( 'cvtech-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-    wp_enqueue_script( 'cvtech-burger', get_template_directory_uri() . '/inc/js/burger.js', array(), _S_VERSION, true );
-
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
-    }
-
-    if(is_page_template( 'template-cgu.php' )) {
-        wp_enqueue_style('cgu-css', get_template_directory_uri() . 'inc/asset/css/cgu.css', array(), _S_VERSION);
     }
 
 }
