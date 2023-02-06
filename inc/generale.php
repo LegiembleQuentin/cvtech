7b2c92c05ebb1672785a22c73f91850a5d6ae2bc
Hyotre <97020497+Hyotre@users.noreply.github.com>
2023-02-06 10:14:16 +0100

    cgur

--- inc/generale.php
+++ inc/generale.php
@@ -97,6 +97,8 @@ add_action( 'widgets_init', 'cvtech_widgets_init' );
 
 function cvtech_scripts() {
     wp_enqueue_style( 'cvtech-style', get_stylesheet_uri(), array(), _S_VERSION );
+    wp_enqueue_style( 'cvtech-style', get_stylesheet_uri(), array(), _S_VERSION );
+
     wp_style_add_data( 'cvtech-style', 'rtl', 'replace' );
 
     wp_enqueue_script( 'cvtech-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
@@ -104,6 +106,10 @@ function cvtech_scripts() {
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
+    }
+
+    if(is_page_template( 'template-cgu.php' )) {
+        wp_enqueue_style('cgu-css', get_template_directory_uri() . '/asset/css/cgu.css', array(), _S_VERSION);
     }
 }
 add_action( 'wp_enqueue_scripts', 'cvtech_scripts' );
---
+++ template-cgu.php
@@ -0,0 +1,56 @@
+<?php
+/* Template Name: cgu */
+get_header();
+?>
+    <section class="color-cgu">
+        <div class="wrap1">
+            <h1 style="font-size: 25px; padding-top: .9rem">Mentions légales</h1>
+            <p>
+                En vigueur au 7 /11/ 022
+                Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004 pour la
+                Confiance dans l’économie numérique, dite L.C.E.N., il est porté à la connaissance des utilisateurs et
+                visiteurs, ci-après l""Utilisateur", du site www.cv-tek.fr , ci-après le "Site", les présentes mentions
+                légales.<br>
+                La connexion et la navigation sur le Site par l’Utilisateur implique acceptation intégrale et sans réserve
+                des présentes mentions légales.
+                Ces dernières sont accessibles sur le Site à la rubrique « Mentions légales ».
+
+                <br><br> <span style="font-size: 25px; padding-top: .9rem">ARTICLE 1 - L'EDITEUR</span><br>
+
+                <br>L’édition et la direction de la publication du Site est assurée par Quentin, domiciliée rouen, dont le
+                numéro de téléphone est 0626993696, et l'adresse e-mail Quentin.legiemble.0301@gmail.com.
+                ci-après l'"Editeur".<br>
+                <br><br><span style="font-size: 25px; padding-top: .9rem">ARTICLE 2 - L'HEBERGEUR</span><br>
+
+                <br>L'hébergeur du Site est la société gitlab, dont le siège social est situé au gitlab.com , avec le numéro
+                de téléphone : 053510045 + adresse mail de contact<br>
+
+                <br><br><span style="font-size: 25px; padding-top: .9rem">ARTICLE 3 - ACCES AU SITE</span><br>
+
+                <br> Le Site est accessible en tout endroit, 7j/7, 24h/24 sauf cas de force majeure, interruption
+                programmée ou non et pouvant découlant d’une nécessité de maintenance.
+                En cas de modification, interruption ou suspension du Site, l'Editeur ne saurait être tenu responsable.
+
+                <br><br><span style="font-size: 25px; padding-top: .9rem"><br>ARTICLE 4 - COLLECTE DES DONNEES</span><br>
+
+                <br>Le Site assure à l'Utilisateur une collecte et un traitement d'informations personnelles dans le respect
+                de la vie privée conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers
+                et aux libertés.<br>
+                En vertu de la loi Informatique et Libertés, en date du 6 janvier 1978, l'Utilisateur dispose d'un droit
+                d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur
+                exerce ce droit :
+                · par mail à l'adresse email Quentin.legiemble.0301@gmail.com
+                · par voie postale au 71600 ;
+                1
+                · via un formulaire de contact ;
+                · via son espace personnel ;
+                Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du Site,
+                sans autorisation de l’Editeur est prohibée et pourra entraînée des actions et poursuites judiciaires
+                telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
+                Rédigé sur http://legalplace.fr
+                2
+            </p>
+        </div>
+    </section>
+<?php
+get_footer();
