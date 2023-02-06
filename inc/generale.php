f2b237b7e3f921ae84ce8816c140e1b9096b5365
LegiembleQuentin <q.legiemble@gmail.com>
2023-02-06 09:26:33 +0100

    update scripts

--- inc/generale.php
+++ inc/generale.php
@@ -97,13 +97,7 @@ add_action( 'widgets_init', 'cvtech_widgets_init' );
 
 function cvtech_scripts() {
     wp_enqueue_style( 'cvtech-style', get_stylesheet_uri(), array(), _S_VERSION );
-    wp_style_add_data( 'cvtech-style', 'rtl', 'replace' );
 
-    wp_enqueue_script( 'cvtech-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-    wp_enqueue_script( 'cvtech-burger', get_template_directory_uri() . '/inc/js/burger.js', array(), _S_VERSION, true );
-
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
+    wp_enqueue_script( 'cvtech-burger', get_template_directory_uri() . '/asset/js/burger.js', array(), _S_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'cvtech_scripts' );
