e8a09d144513602b02acb279ed55df2350cde70f
Hyotre <97020497+Hyotre@users.noreply.github.com>
2023-02-06 11:12:49 +0100

    mention

---
+++ template-mentionlegale.php
@@ -0,0 +1,37 @@
+<?php
+/* Template Name: mention */
+get_header();
+?>
+    <div class="color-cgu text-center">
+        <div class="wrap1">
+            <h2 class="title-mention">Les Mentions Légales</h2>
+
+            <div class="mention1">
+                <h4 class="sous-title-mention">Identification</h4>
+                <div class="underline"></div>
+                <div class="paragraphe">
+                    <p>Nom de l'administrateur principal : Legiemble</p>
+                    <p>Direction de l'information légale et administrative : 10 rue du Général Sarrail 76000 Rouen</p>
+                    <p>Standard : 01 23 45 56 78</p>
+                    <p>N° SIREN : 012-345-678</p>
+                    <p>N° SIRET : 012-345-678 01234</p>
+                    <p class="">Code APE : 1234A / Secteur de santé</p>
+                </div>
+            </div>
+            <div class="mention2">
+                <h4 class="sous-title-mention">Traitement des données à caractère personnel</h4>
+                <div class="underline"></div>
+                <div class="spoofer" >
+                    <p class="spoofer">cv-tech a fait l'objet d'une déclaration à la Commission Nationale de l'Informatique et des Libertés (CNIL).
+                        La base de données diffusée dans la rubrique Annuaire de l'administration a fait l'objet d'une déclaration spécifique.
+                        L'annuaire de l'administration a été autorisé par l'arrêté du 6 novembre 2000 relatif à la création d'un site sur internet.
+                        Conformément aux dispositions de la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données qui vous concernent. Pour demander une modification, rectification ou suppression des données vous concernant, il vous suffit d'envoyer un courrier par voie électronique ou postale à la DILA en justifiant de votre identité.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php
+get_footer();
