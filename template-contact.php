cfd6f311ccd61c8e37e9fef9ee0089e5750e764a
lucas <lucaseliot363@gmail.com>
2023-02-07 12:29:30 +0100

    contact

--- template-contact.php
+++ template-contact.php
@@ -2,7 +2,19 @@
 /*Template Name: contact*/
 get_header(); ?>
 
-
+<form method="post" id="formulaire">
+    <div class="mail">
+        <label for="mail" id="mail"></label>
+        <input type="email" id="email" placeholder="votre mail">
+        <span class="error"></span>
+    </div>
+    <div class="message">
+        <label for="message"></label>
+        <textarea id="message" placeholder="message"></textarea>
+        <span class="error"></span>
+    </div>
+    <input type="submit" value="Envoyer">
+</form>
 
 
 <?php get_footer() ?>
