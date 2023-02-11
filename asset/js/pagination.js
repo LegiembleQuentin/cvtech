dbadc6d95f82313bd4472f05b8c68fd3ce120a05
Hyotre <97020497+Hyotre@users.noreply.github.com>
2023-02-11 15:25:38 +0100

    contzctu

---
+++ asset/js/pagination.js
@@ -0,0 +1,51 @@
+let currentPage = 1;
+let itemsPerPage = 10;
+let pageNumbers = [];
+
+const list = document.getElementsByClassName("person-box");
+const pagination = document.getElementById("pagination");
+
+for (let i = 1; i <= list.length; i++) {
+    pageNumbers.push(i);
+}
+
+const showPage = (list, page) => {
+    for (let i = 0; i < list.length; i++) {
+        list[i].style.display = "none";
+    }
+
+    let startIndex = (page - 1) * itemsPerPage;
+    let endIndex = Math.min(startIndex + itemsPerPage - 1, list.length - 1);
+    console.log("Nombre d'éléments affichés : ", document.querySelectorAll(".person-box[style='display: block;']").length);
+    console.log("endIndex:", endIndex);
+    console.log("startIndex:", startIndex);
+    for (let i = startIndex; i <= endIndex; i++) {
+        list[i].style.display = "block";
+    }
+};
+const appendPageLinks = (list) => {
+    if (pagination) {
+        pagination.innerHTML = "";
+        let pages = Math.ceil(list.length / itemsPerPage);
+
+        for (let i = 1; i <= pages; i++) {
+            let button = document.createElement("button");
+            button.innerText = i;
+            if (currentPage === i) {
+                button.classList.add("active");
+            }
+
+            button.addEventListener("click", (event) => {
+                currentPage = event.target.innerText;
+                showPage(list, currentPage);
+                let active = document.querySelector(".active");
+                active.classList.remove("active");
+                event.target.classList.add("active");
+            });
+            pagination.appendChild(button);
+        }
+    }
+};
+
+showPage(list, currentPage);
+appendPageLinks(list)
--- template-listingCV.php
+++ template-listingCV.php
@@ -5,9 +5,44 @@ get_header(); ?>
     <section  class="listing wrap1">
             <div id="recruteur">
             </div>
+        <?php
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "wp-cvtech";
+
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        if (!$conn) {
+            die("La connexion a échoué: " . mysqli_connect_error());
+        }
+
+        if (isset($_POST['search'])) {
+            $search = $_POST['search'];
+            $sql = "SELECT * FROM cvtc_skill WHERE name LIKE '%$search%'";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+                $num_rows = mysqli_num_rows($result);
+                echo "<div class='padding-top-listing'>";
+                echo "Résultats de la recherche pour la compétence recherchée :<br>";
+                echo "Nombre de compétences trouvées : " . $num_rows . "<br>";
+
+            } else {
+                echo "Aucun résultat trouvé pour la compétence recherchée.";
+            }
+        }
+
+        mysqli_close($conn);
+        ?>
+
         <form action="" method="post">
             <input type="text" name="search">
             <button type="submit">Rechercher</button>
         </form>
     </section>
+    <div id="pagination" class="pagination">
+    </div>
+
 <?php get_footer();
