f34201dd881e1cf0598872c449ac57da858c6ebe
Petyth_prince <petythprincebely@gmail.com>
2023-02-13 09:26:00 +0100

    push de la page template-listingCv

--- asset/js/listingcv.js
+++ asset/js/listingcv.js
@@ -1,147 +1,98 @@
-
 const recruteur = document.querySelector('#recruteur');
+const divModal = document.querySelector('.content_txt');
+const div_Modal = document.querySelector('#modal1')
+const modalClose = document.querySelector('.modal-close');
 
+const dropdownToggle = document.querySelector('.nav-link.dropdown-toggle');
+const dropdownMenu = document.querySelector('.dropdown-menu');
 
 let params = new FormData();
-params.append('action', 'get_Cv_data')
+params.append('action', 'get_Cv_data');
 
 fetch(MYSCRIPT.ajaxUrl, {
     method: 'post',
     body: params
-}).then(function(response) {
-    return response.json()
-}).then(function(data) {
-    console.log(data);
-    data.forEach(function(donnee) {
-        const container = document.createElement('div');
-        container.classList.add('person-box')
-        const nom = document.createElement('p');
-        nom.innerText = 'Nom : ';
-        const span_nom = document.createElement('span');
-        span_nom.classList.add('name_listingcv');
-        nom.appendChild(span_nom);
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        data.forEach(donnee => {
+            const container = document.createElement('div');
+            container.classList.add('person-box');
+            const divImg = document.createElement('div');
+            divImg.classList.add('div-bonhome-cv');
+            const img = document.createElement('img');
+            img.src = MYSCRIPT.logo_inscription;
+            divImg.appendChild(img);
+            container.appendChild(divImg);
 
-        const div_img = document.createElement('div')
-        div_img.classList.add('div-bonhome-cv')
-        const img = document.createElement('img');
-        img.src = MYSCRIPT.logo_inscription;
-        div_img.appendChild(img);
+            const link = document.createElement('a');
+            link.href = "#";
+            link.innerText = 'Voir plus';
+            link.classList.add('link_modal');
 
+            const details = [
+                {text: 'Nom : ', property: 'name'},
+                {text: 'Prenom : ', property: 'firstname'},
+                {text: 'Job recherché : ', property: 'job'},
+                {text: 'Téléphone : ', property: 'phone'},
+                {text: 'Email : ', property: 'mail'},
+                {text: 'Age : ', property: 'age'},
+                {text: 'Permis : ', property: 'permis'}
+            ];
 
-        const a = document.createElement('a');
-        a.href = "#";
-        a.innerText = 'Voir plus';
+            const detailsDiv = document.createElement('div');
 
-        container.appendChild(div_img);
-        container.append(nom);
+            details.forEach(({text, property}) => {
+                const p = document.createElement('p');
+                p.innerText = text;
+                const span = document.createElement('span');
+                span.classList.add(`${property}_listingcv`);
+                span.innerText = donnee[property] || 'pas renseigné';
+                p.appendChild(span);
+                detailsDiv.appendChild(p);
+            });
 
-        const prenom = document.createElement('p');
-        prenom.innerText = 'Prenom : ';
-        const span_prenom = document.createElement('span');
-        span_prenom.classList.add('firstname_listingcv');
-        prenom.appendChild(span_prenom);
-        container.append(prenom);
+            container.appendChild(detailsDiv);
+            container.appendChild(link);
+            div_Modal.style.display = "none";
 
-        const job = document.createElement('p');
-        job.innerText = 'Job recherché : ';
-        const span_job = document.createElement('span');
-        span_job.classList.add('job_listingcv');
-        job.appendChild(span_job);
-        container.append(job);
+            detailsDiv.addEventListener('click', () => {
+                div_Modal.style.display = "block";
+                divModal.style.display = "block";
+                divModal.innerHTML = '';
+                div_Modal.style.display = '';
+                divModal.appendChild(detailsDiv.cloneNode(true));
+            });
 
-        const phone = document.createElement('p');
-        phone.innerText = 'Téléphone : ';
-        const span_phone = document.createElement('span');
-        span_phone.classList.add('phone_listingcv');
-        phone.appendChild(span_phone);
-        container.append(phone);
+            link.addEventListener('click', () => {
+                div_Modal.style.display = "block";
+                divModal.style.display = "block";
+                divModal.innerHTML = '';
+                div_Modal.style.display = '';
+                divModal.appendChild(detailsDiv.cloneNode(true));
+            });
 
+            recruteur.appendChild(container);
+        });
 
-        const mail = document.createElement('p');
-        mail.innerText = 'Email : ';
-        const span_mail = document.createElement('span');
-        span_mail.classList.add('mail_listingcv');
-        mail.appendChild(span_mail);
-        container.append(mail);
-
-
-        const age = document.createElement('p');
-        age.innerText = 'age : ';
-        const span_age = document.createElement('span');
-        span_age.classList.add('age_listingcv');
-        age.appendChild(span_age);
-        container.append(age);
-
-
-        const permis = document.createElement('p');
-        permis.innerText = 'Permis : ';
-        const span_permis = document.createElement('span');
-        span_permis.classList.add('permis_listingcv');
-        permis.appendChild(span_permis);
-        container.append(permis);
-
-        a.classList.add('.modal')
-        container.appendChild(a)
-        recruteur.appendChild(container);
-
-        if (donnee.name!= ''){
-            span_nom.innerText = donnee.name;
-
-        }
-        else {
-            span_nom.innerText = 'pas renseigné';
-        }
-        if (donnee.firstname!= ''){
-            span_prenom.innerText = donnee.firstname;
-        }
-        else{
-            span_prenom.innerText = 'pas renseigné';
-        }
-        if(donnee.job!= ''){
-            span_job.innerText = donnee.job;
-        }
-        else{
-            span_job.innerText = 'pas renseigné';
-        }
-        if(donnee.phone!= ''){
-            span_phone.innerText = donnee.phone;
-        }
-        else {
-            span_phone.innerText = 'pas renseigné';
-        }
-        if (donnee.email!= '') {
-            span_mail.innerText = donnee.email;
-        }else {
-            span_mail.innerText = 'pas renseigné';
-        }
-
-        if (donnee.age!== null && donnee.permis!== null){
-            span_age.innerText = donnee.age;
-            span_permis.innerText = donnee.permis;
-        }else {
-            span_age.innerText = 'pas renseigné';
-            span_permis.innerText = 'pas renseigné';
-        }
+        modalClose.addEventListener('click', () => {
+            div_Modal.style.display = "none";
+            dropdownMenu.style.display = "none";
+        });
     });
 
-});
 
+dropdownToggle.addEventListener('click', function() {
+    if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+        dropdownMenu.style.opacity = 0;
+        dropdownMenu.style.transform = "translateY(-20px)";
 
-document.querySelectorAll('.modal').forEach(function(modal) {
-    modal.addEventListener('click', function(e) {
-        let currentModal = document.querySelector('.modal[style="display: block;"]');
-        if (currentModal && !currentModal.contains(e.target)) {
-            currentModal.style.display = "none";
-        }
-        let modalId = this.parentNode.getAttribute('href');
-        document.querySelector(modalId).style.display = "block";
-    });
-});
-
-document.querySelectorAll('.close').forEach(function(close) {
-    close.addEventListener('click', function() {
-        let modal = close.closest('.modal');
-        modal.style.display = "none";
-    });
+    } else {
+        dropdownMenu.style.display = "block";
+        dropdownMenu.style.opacity = 1;
+        dropdownMenu.style.transform = "translateY(0px)";
+    }
 });
 
--- template-listingCV.php
+++ template-listingCV.php
@@ -2,12 +2,37 @@
 /* Template Name: listingCV */
 
 get_header(); ?>
-    <section  class="listing wrap1">
-            <div id="recruteur">
-            </div>
+    <section  class="listing">
+        <div class="container">
         <form action="" method="post">
             <input type="text" name="search">
             <button type="submit">Rechercher</button>
         </form>
+            <div id="recruteur" class="wrap1">
+            </div>
+        <div id="modal1" class="modal wrap1">
+            <div class="modal-content">
+                <div class="title">
+                    <h4 class="modal-header">Informations supplémentaires</h4>
+                    <div class="link">
+                        <a class="nav-link dropdown-toggle" href="#" id="optionsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="optionsDropdown">
+                            <a class="dropdown-item" href="#">Télécharger le CV</a>
+                            <a class="dropdown-item" href="#">Supprimer le CV</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="content_txt"></div>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="modal-close waves-effect waves-green btn-flat">Fermer</a>
+            </div>
+        </div>
+        </div>
     </section>
 <?php get_footer();
+
+
+
